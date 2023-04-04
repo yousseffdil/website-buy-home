bc592bfc2ad2303fc0b5094505d22ba476b76378
yousseffdil <60134214+yousseffdil@users.noreply.github.com>
2023-04-04 15:51:43 +0200

    animation_scroll

---
+++ source/main.js
@@ -0,0 +1,56 @@
+ScrollReveal().reveal('.info', {
+    delay: 200,
+    origin: 'left',
+    distance: '500px',
+    duration: 500,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.headImg', {
+    delay: 200,
+    origin: 'right',
+    distance: '500px',
+    duration: 500,
+    easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.secondinfo', {
+    delay: 200,
+    origin: 'right',
+    distance: '500px',
+    duration: 500,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.secondinfo', {
+    delay: 200,
+    origin: 'right',
+    distance: '500px',
+    duration: 500,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.secondimg', {
+    delay: 200,
+    origin: 'left',
+    distance: '500px',
+    duration: 500,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.thirdinfo', {
+    delay: 200,
+    origin: 'left',
+    distance: '500px',
+    duration: 500,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.thirdimage', {
+    delay: 200,
+    origin: 'right',
+    distance: '500px',
+    duration: 500,
+    easing: 'ease-in-out',
+});
